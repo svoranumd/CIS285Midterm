271f957b911c024187675ec090bf4fbc3ba9b29b
svoranumd <79284398+svoranumd@users.noreply.github.com>
2021-03-09 13:29:21 -0500

    Update addintegers2.java

---
+++ addintegers2.java
@@ -0,0 +1,13 @@
+public class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 14;
+        int second = 26;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
